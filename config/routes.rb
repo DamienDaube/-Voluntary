13950e486eb023dfa97254cf906f11118ed88ea9
Noam Say <noamsay@gmail.com>
2018-02-19 18:44:52 +0100

    service controller index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :edit, :update]
+
+  resources :services do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :bookings, except: [:new, :create, :edit]
+
+  root to: 'pages#home'
+
 end
