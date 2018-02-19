70d22b39d3e1a2b324c96a8a40cf2920bed1e8b5
NM <nm.spamming@gmail.com>
2018-02-19 18:15:23 +0100

    regenerate all migrations

--- db/migrate/20180219170733_create_bookings.rb
+++ db/migrate/20180219170733_create_bookings.rb
@@ -1,13 +1,12 @@
 class CreateBookings < ActiveRecord::Migration[5.1]
   def change
     create_table :bookings do |t|
-      t.references :user, foreign_key: true
-      t.references :service, foreign_key: true
       t.date :start_date
       t.date :end_date
-      t.integer :price
-      t.boolean :paid
       t.string :status
+      t.boolean :paid
+      t.references :user, foreign_key: true
+      t.references :service, foreign_key: true
 
       t.timestamps
     end
