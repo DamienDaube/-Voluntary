30bf47042d8c0d045f531af7fef7d875b7d96782
NM <nm.spamming@gmail.com>
2018-02-20 12:27:52 +0100

    completed seed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
-  USER_ROLES = ["beneficiary", "sponsor", "provider", "admin"]
+  # USER_ROLES = ["beneficiary", "sponsor", "provider", "admin"]
 
-  before_action :set_user, only: [:show, :edit, :update]
+  # before_action :set_user, only: [:show, :edit, :update]
 
-  def show
-    authorize @user
-  end
+  # def show
+  #   authorize @user
+  # end
 
-  def edit
-    authorize @user
-  end
+  # def edit
+  #   authorize @user
+  # end
 
-  def update
-    @user.update(user_params)
-    authorize @user
-  end
+  # def update
+  #   @user.update(user_params)
+  #   authorize @user
+  # end
 
-  private
+  # private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
-  end
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :email)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :users, only: [:show, :edit, :update]
 
   resources :services do
     resources :bookings, only: [:new, :create]
