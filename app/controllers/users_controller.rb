f075c2ed98aee8ee6c7d4a97c37b9bfbc8f819db
DamienDaube <damien.daube@gmail.com>
2018-02-23 13:22:54 +0100

    changecloud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :role)
+    params.require(:user).permit(:first_name, :last_name, :email, :role, :photo)
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :photo, PhotoUploader
+
   def provider_bookings
     services.map { |service| service.bookings}.flatten
   end
